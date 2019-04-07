0eb9bab418190fc19b47505159b3e35f5ed603f4
服部翼 <hattoritsubasa@hattoritsubasanomacbook-pro.local>
2019-04-07 23:51:30 +0900

    Initial Commit

---
+++ oekaki2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oekaki2
+//
+//  Created by 服部翼 on 2019/04/07.
+//  Copyright © 2019 服部翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
